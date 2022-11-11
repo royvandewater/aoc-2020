b78e1b3e1c347114bb0628377d4880c5bc0e2848
Roy van de Water <roy.v.water@gmail.com>
2022-11-11 10:09:24 -0700

    Another checkpoint before the re-write

--- day20/src/stage_1/mod.rs
+++ day20/src/stage_1/mod.rs
@@ -54,56 +54,149 @@ impl Stage1 {
     }
 
     fn solve(&self) -> Result<(), String> {
-        if self.layout.borrow().iter().any(have_no_options) {
-            return Err("Couldn't solve the layout".into());
-        }
-        if self.layout.borrow().iter().all(have_one_option) {
+        self.print_known_tiles();
+        if self.is_solved() {
             return Ok(());
         }
 
         let reduced_layout = reduce_layout(self.layout.borrow())?;
-        if total_entropy(&reduced_layout) >= total_entropy(&self.layout.borrow()) {
-            return Err("Failed to reduce entropy".into());
+        let old_entropy = total_entropy(&self.layout.borrow());
+        let new_entropy = total_entropy(&reduced_layout);
+
+        println!("{} -> {}", old_entropy, new_entropy);
+
+        if old_entropy <= new_entropy {
+            return Err(format!(
+                "Failed to reduce entropy: {} == {}",
+                old_entropy, new_entropy
+            ));
+        }
+
+        if new_entropy == 0 {
+            return Err(format!(
+                "0 entropy detected: {}, {}",
+                old_entropy, new_entropy
+            ));
         }
 
         self.layout.replace(reduced_layout);
         self.solve()
+    }
+
+    fn is_solved(&self) -> bool {
+        let dimensions = self.dimensions();
+
+        for x in -dimensions..=0 {
+            for y in -dimensions..=0 {
+                if self.solved_square_at(&[x, y], dimensions) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    fn solved_square_at(&self, position: &[isize; 2], dimensions: isize) -> bool {
+        let &[startx, starty] = position;
+        let [endx, endy] = [startx + dimensions, starty + dimensions];
+        let layout = self.layout.borrow();
+
+        for x in startx..endx {
+            for y in starty..endy {
+                if 1 != layout.get(&[x, y]).unwrap().len() {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    fn print_known_tiles(&self) {
+        let dimensions = self.dimensions();
+        let layout = self.layout.borrow();
+
+        print!("\n");
+        for x in -dimensions..=dimensions {
+            for y in -dimensions..=dimensions {
+                let options = layout.get(&[x, y]).unwrap();
+                match options.len() {
+                    x if x < 10 => print!("{}", x),
+                    _ => print!("."),
+                }
+            }
+            print!("\n");
+        }
     }
 }
 
 fn reduce_layout(
     layout: std::cell::Ref<HashMap<[isize; 2], HashSet<Tile>>>,
 ) -> Result<HashMap<[isize; 2], HashSet<Tile>>, String> {
-    let mut new_layout = HashMap::new();
+    Ok(layout
+        .iter()
+        .map(|(position, tiles)| {
+            (
+                position.clone(),
+                reduce_tiles_for_position(&layout, position, tiles),
+            )
+        })
+        .collect())
+}
 
-    for (position, tiles) in layout.iter() {
-        new_layout.insert(
-            position.clone(),
-            tiles
-                .iter()
-                .filter(|tile| is_valid_tile(&layout, position, tile))
-                .cloned()
-                .collect(),
-        );
-    }
-
-    return Ok(new_layout);
+fn reduce_tiles_for_position(
+    layout: &std::cell::Ref<HashMap<[isize; 2], HashSet<Tile>>>,
+    position: &[isize; 2],
+    tiles: &HashSet<Tile>,
+) -> HashSet<Tile> {
+    tiles
+        .iter()
+        .filter(|tile| is_valid_tile(&layout, position, tile))
+        .cloned()
+        .collect()
 }
 
 fn is_valid_tile(
     layout: &HashMap<[isize; 2], HashSet<Tile>>,
-    position: &[isize; 2],
+    &[x, y]: &[isize; 2],
     tile: &Tile,
 ) -> bool {
-    todo!()
-}
+    if x == 1 && y == 0 && tile.id == 2 {
+        println!("Debugger");
+    }
 
-fn have_one_option<T>((_, options): (T, &HashSet<Tile>)) -> bool {
-    options.len() == 1
-}
+    let mut valid_edges = 0;
 
-fn have_no_options<T>((_, options): (T, &HashSet<Tile>)) -> bool {
-    options.len() == 0
+    let north = [x, y + 1];
+    if let Some(other) = layout.get(&north) {
+        if other.iter().any(|other| tile.edges[0] == other.edges[2]) {
+            valid_edges += 1;
+        }
+    }
+
+    let east = [x + 1, y];
+    if let Some(other) = layout.get(&east) {
+        if other.iter().any(|other| tile.edges[1] == other.edges[3]) {
+            valid_edges += 1;
+        }
+    }
+
+    let south = [x, y - 1];
+    if let Some(other) = layout.get(&south) {
+        if other.iter().any(|other| tile.edges[2] == other.edges[0]) {
+            valid_edges += 1;
+        }
+    }
+
+    let west = [x - 1, y];
+    if let Some(other) = layout.get(&west) {
+        if other.iter().any(|other| tile.edges[3] == other.edges[1]) {
+            valid_edges += 1;
+        }
+    }
+
+    return valid_edges >= 2;
 }
 
 fn total_entropy(layout: &HashMap<[isize; 2], HashSet<Tile>>) -> usize {
@@ -127,11 +220,17 @@ mod tests {
     #[test]
     fn test_simple() -> Result<(), String> {
         let sut: Stage1 = Stage1 {
+            // layout:
+            // 1 <0>  2
+            // ^      ^
+            // 1      2
+            // v      v
+            // 4 <3>  5
             tiles: vec![
-                Tile { id: 1, edges: ["..".into(), ".#".into(), "#.".into(), "##".into()] },
-                Tile { id: 2, edges: ["..".into(), ".#".into(), "#.".into(), "##".into()] },
-                Tile { id: 3, edges: ["..".into(), ".#".into(), "#.".into(), "##".into()] },
-                Tile { id: 4, edges: ["..".into(), ".#".into(), "#.".into(), "##".into()] },
+                Tile { id: 1, edges: ["n.".into(), "..".into(), ".#".into(), "w.".into()] },
+                Tile { id: 2, edges: ["n.".into(), "e.".into(), "#.".into(), "..".into()] },
+                Tile { id: 3, edges: [".#".into(), "##".into(), "s.".into(), "w.".into()] },
+                Tile { id: 4, edges: ["#.".into(), "e.".into(), "s.".into(), "##".into()] },
             ],
             ..Default::default()
         };
