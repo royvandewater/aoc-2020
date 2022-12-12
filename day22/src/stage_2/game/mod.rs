d6657e09061d17ba392c897763014a85a37fde86
Roy van de Water <roy.v.water@gmail.com>
2022-12-12 10:26:05 -0700

    Day 22, part 2

--- day22/src/stage_2/game/mod.rs
+++ day22/src/stage_2/game/mod.rs
@@ -1,7 +1,8 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    hash::Hash,
-};
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::Hash;
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hasher;
 
 #[derive(Debug, PartialEq)]
 pub enum Winner {
@@ -16,50 +17,77 @@ pub struct State {
 }
 
 pub fn game(deck_1: Vec<usize>, deck_2: Vec<usize>) -> (Winner, Vec<usize>) {
-    let (winner, deck) = game_inner(deck_1.into(), deck_2.into(), HashSet::new());
+    let (winner, deck) = game_inner(deck_1.into(), deck_2.into(), &mut HashMap::new());
 
     (winner, deck.into())
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
 }
 
 fn game_inner(
     mut deck_1: VecDeque<usize>,
     mut deck_2: VecDeque<usize>,
-    mut known_states: HashSet<State>,
+    mut known_games: &mut HashMap<u64, Winner>,
 ) -> (Winner, VecDeque<usize>) {
-    let state = State { deck_1: deck_1.clone(), deck_2: deck_2.clone() };
-    if known_states.contains(&state) {
-        return (Winner::Player1, deck_1);
+    let game_state = calculate_hash(&(&deck_1, &deck_2));
+
+    if let Some(winner) = known_games.get(&game_state) {
+        return match winner {
+            Winner::Player1 => (Winner::Player1, deck_1),
+            Winner::Player2 => (Winner::Player2, deck_2),
+        };
     }
-    known_states.insert(state);
 
-    let card_1 = deck_1.pop_front().unwrap();
-    let card_2 = deck_2.pop_front().unwrap();
+    let mut known_states: HashSet<u64> = HashSet::new();
 
-    let winner = match both_players_have_enough_cards(&card_1, &deck_1, &card_2, &deck_2) {
-        true => game_inner(deck_1.clone(), deck_2.clone(), HashSet::new()).0,
-        false => single_round(&card_1, &card_2),
-    };
-
-    match winner {
-        Winner::Player1 => {
-            deck_1.push_back(card_1);
-            deck_1.push_back(card_2);
+    loop {
+        let state = calculate_hash(&(&deck_1, &deck_2));
+        if known_states.contains(&state) {
+            known_games.insert(game_state, Winner::Player1);
+            return (Winner::Player1, deck_1);
         }
-        Winner::Player2 => {
-            deck_2.push_back(card_2);
-            deck_2.push_back(card_1);
+        known_states.insert(state);
+
+        let card_1 = deck_1.pop_front().unwrap();
+        let card_2 = deck_2.pop_front().unwrap();
+
+        let winner = match both_players_have_enough_cards(&card_1, &deck_1, &card_2, &deck_2) {
+            true => {
+                game_inner(
+                    deck_1.iter().take(card_1).cloned().collect(),
+                    deck_2.iter().take(card_2).cloned().collect(),
+                    &mut known_games,
+                )
+                .0
+            }
+            false => single_round(&card_1, &card_2),
+        };
+
+        match winner {
+            Winner::Player1 => {
+                deck_1.push_back(card_1);
+                deck_1.push_back(card_2);
+            }
+            Winner::Player2 => {
+                deck_2.push_back(card_2);
+                deck_2.push_back(card_1);
+            }
+        }
+
+        if deck_2.is_empty() {
+            known_games.insert(game_state, Winner::Player1);
+            return (Winner::Player1, deck_1.into());
+        }
+
+        if deck_1.is_empty() {
+            known_games.insert(game_state, Winner::Player2);
+            return (Winner::Player2, deck_2.into());
         }
     }
-
-    if deck_2.is_empty() {
-        return (Winner::Player1, deck_1.into());
-    }
-
-    if deck_1.is_empty() {
-        return (Winner::Player2, deck_2.into());
-    }
-
-    return game_inner(deck_1, deck_2, known_states);
 }
 
 fn both_players_have_enough_cards(
@@ -68,7 +96,7 @@ fn both_players_have_enough_cards(
     &card_2: &usize,
     deck_2: &VecDeque<usize>,
 ) -> bool {
-    card_1 <= deck_1.len() && card_2 <= deck_2.len()
+    deck_1.len() >= card_1 && deck_2.len() >= card_2
 }
 
 fn single_round(card_1: &usize, card_2: &usize) -> Winner {
